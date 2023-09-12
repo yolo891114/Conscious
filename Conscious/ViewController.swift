c5e24c172f8984cf368e8f538845171b517c386d
jeff <omailong@ssy.local>
2023-09-12 11:24:25 +0800

    First commit

---
+++ Conscious/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Conscious
+//
+//  Created by jeff on 2023/9/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
