3a54c1d20ee4193f06fe130f9eaa939feca5117e
jeff <omailong@ssy.local>
2023-09-19 15:42:48 +0800

    FEAT: Line charts established

--- Conscious/EmotionResultPage/View/EmotionDataViewController.swift
+++ Conscious/EmotionResultPage/View/EmotionDataViewController.swift
@@ -9,42 +9,112 @@ import Foundation
 import UIKit
 import SwiftUI
 import Charts
+import Combine
 
 class EmotionDataViewController: UIViewController {
+
+    @IBOutlet weak var createButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    var emotionData: [EmotionRecord] = []
+
+    private var canAddNewRecord: Bool?
+
+    lazy var viewModel = EmotionDataViewModel()
+
+    private var cancellables = Set<AnyCancellable>()
+
+    @IBAction func createButtonTapped(_ sender: UIButton) {
+        if let canAddNewRecord = canAddNewRecord, canAddNewRecord == false {
+            showOverrideAlert()
+        }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+
+        viewModel.canAddNewEmotionRecord()
+
+        viewModel.fetchEmotionRecords()
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .failure(let error):
+                        print("Error: \(error.localizedDescription)")
+                    case .finished:
+                        break
+                    }
+                }, receiveValue: { data in
+                    self.emotionData = data
+                    print(data)
+                }
+            ).store(in: &cancellables)
+
+        viewModel.$canAddNewRecord
+            .sink { canAddNewRecord in
+                self.canAddNewRecord = canAddNewRecord
+            }
+            .store(in: &cancellables)
+
+        viewModel.$currentViewingMonth
+            .sink { newMonth in
+                print("---")
+                print(newMonth)
+                self.dateLabel.text = String(describing: newMonth)
+            }
+            .store(in: &cancellables)
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let swiftUIView = EmotionLineChartView()
+        let swiftUIView = EmotionLineChartView(viewModel: viewModel)
 
         let hostingController = UIHostingController(rootView: swiftUIView)
 
         self.addChild(hostingController)
 
-        hostingController.view.frame = self.view.bounds
+        //        hostingController.view.frame = self.view.bounds
         hostingController.view.translatesAutoresizingMaskIntoConstraints = false
 
         self.view.addSubview(hostingController.view)
 
         NSLayoutConstraint.activate([
-            hostingController.view.topAnchor.constraint(equalTo: self.view.topAnchor),
-            hostingController.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
-            hostingController.view.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-            hostingController.view.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+            hostingController.view.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 16),
+            hostingController.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 16),
+            hostingController.view.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
+            hostingController.view.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16)
         ])
 
         hostingController.didMove(toParent: self)
+
+        self.view.bringSubviewToFront(createButton)
+        self.view.bringSubviewToFront(dateLabel)
     }
+
+    func showOverrideAlert() {
+        let alert = UIAlertController(title: "本週已有資料存在", message: "確定要覆蓋本週資料嗎？", preferredStyle: .alert)
+
+        alert.addAction(UIAlertAction(title: "確定", style: .default, handler: { _ in
+            self.viewModel.deleteCurrentWeekEmotionRecord()
+            self.performSegue(withIdentifier: "showSurveySegue", sender: self)
+        }))
+
+        alert.addAction(UIAlertAction(title: "取消", style: .cancel, handler: nil))
+
+        self.present(alert, animated: true, completion: nil)
+    }
+
 }
 
 struct EmotionLineChartView: View {
-    let emotionRecord = [EmotionRecord(emotionRecordID: UUID().uuidString, emotionScore: 32, date: Date()),
-                         EmotionRecord(emotionRecordID: UUID().uuidString, emotionScore: 21, date: Date())]
+
+    @ObservedObject var viewModel: EmotionDataViewModel
 
     var body: some View {
         VStack {
             Chart {
-                ForEach(emotionRecord) { item in
+                ForEach(viewModel.currentMonthRecords) { item in
                     LineMark(
                         x: .value("Date", item.date),
                         y: .value("Score", item.emotionScore))
@@ -52,5 +122,20 @@ struct EmotionLineChartView: View {
             }
             .frame(height: 300)
         }
+        .gesture(
+            DragGesture()
+                .onEnded { value in // 判斷位移量
+                    let horizontalValue = value.translation.width
+                    let verticalValue = value.translation.height
+
+                    if abs(horizontalValue) > abs(verticalValue) {
+                        if horizontalValue > 0 {
+                            viewModel.switchToPreviousMonth()
+                        } else {
+                            viewModel.switchToNextMonth()
+                        }
+                    }
+                }
+        )
     }
 }
--- Conscious/EmotionResultPage/ViewModel/EmotionDataViewModel.swift
+++ Conscious/EmotionResultPage/ViewModel/EmotionDataViewModel.swift
@@ -10,5 +10,81 @@ import Combine
 
 class EmotionDataViewModel: ObservableObject {
 
+    @Published var canAddNewRecord: Bool?
+    @Published var emotionRecords: [EmotionRecord] = []
+    @Published var currentViewingMonth: Int = Calendar.current.component(.month, from: Date())
+    @Published var currentViewingYear: Int = Calendar.current.component(.year, from: Date())
+
+    private var cancellables = Set<AnyCancellable>()
+
+    var currentMonthRecords: [EmotionRecord] {
+        let calendar = Calendar.current
+        return emotionRecords.filter {
+            let recordMonth = calendar.component(.month, from: $0.date)
+            let recordYear = calendar.component(.year, from: $0.date)
+
+            return recordMonth == currentViewingMonth && recordYear == currentViewingYear
+        }
+    }
+
+    var currentYearRecords: [EmotionRecord] {
+        let calendar = Calendar.current
+        return emotionRecords.filter {
+            let recordYear = calendar.component(.year, from: $0.date)
+
+            return recordYear == currentViewingYear
+        }
+    }
 }
 
+// MARK: - Function
+
+extension EmotionDataViewModel {
+
+    func canAddNewEmotionRecord() {
+        FirebaseManager.shared.canAddRecordThisWeek(user: "no1") { canAddNewRecord in
+            self.canAddNewRecord = canAddNewRecord
+        }
+    }
+
+    func deleteCurrentWeekEmotionRecord() {
+        FirebaseManager.shared.deleteCurrentWeekEmotionRecord(user: "no1")
+        self.canAddNewRecord = true
+    }
+
+    func switchToPreviousMonth() {
+        if currentViewingMonth > 1 {
+            currentViewingMonth -= 1
+        } else {
+            currentViewingMonth = 12
+            currentViewingYear -= 1
+        }
+    }
+
+    func switchToNextMonth() {
+        if currentViewingMonth < 12 {
+            currentViewingMonth += 1
+        } else {
+            currentViewingMonth = 1
+            currentViewingYear += 1
+        }
+    }
+
+    // Fetch 後回傳 Future promise
+    func fetchEmotionRecords() -> Future<[EmotionRecord], Error> {
+        return Future { promise in
+            FirebaseManager.shared.fetchEmotionRecords(user: "no1") { emotionRecords, error in
+                if let error = error {
+                    print("Error fetching emotion record: \(error.localizedDescription)")
+                    promise(.failure(error))
+                }
+
+                if let emotionRecords = emotionRecords {
+                    self.emotionRecords = emotionRecords
+                    promise(.success(emotionRecords))
+                }
+            }
+        }
+    }
+
+}
