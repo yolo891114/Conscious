f41fcad6358cefbb21b1433b37eaff62dce8532d
jeff <omailong@ssy.local>
2023-09-21 11:20:32 +0800

    FEAT: Caculate consecutive punch day successfully

---
+++ Conscious/EnterPasswordPage/Model/PasswordManager.swift
@@ -0,0 +1,46 @@
+//
+//  PasswordManager.swift
+//  Conscious
+//
+//  Created by jeff on 2023/9/21.
+//
+
+import Foundation
+import Security
+
+class PasswordManager {
+
+    func savePassword(password: String) {
+        let passwordData = password.data(using: .utf8)!
+
+        let query: [String: Any] = [
+            kSecValueData as String: passwordData,
+            kSecAttrService as String: "ConsciousDiary",
+            kSecClass as String: kSecClassGenericPassword]
+
+        let status = SecItemAdd(query as CFDictionary, nil)
+
+        print("status: \(status)")
+    }
+
+    func getPassword() -> String? {
+
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: "ConsciousDiary",
+            kSecReturnData as String: kCFBooleanTrue!]
+
+        var returnData: AnyObject?
+
+        let status = SecItemCopyMatching(query as CFDictionary, &returnData)
+
+        // 判斷操作成功與否
+        if status == errSecSuccess {
+            if let data = returnData as? Data,
+               let password = String(data: data, encoding: .utf8) {
+                    return password
+                }
+        }
+        return nil
+    }
+}
---
+++ Conscious/PunchRecordPage/View/PunchRecordViewController.swift
@@ -0,0 +1,86 @@
+//
+//  PunchRecordViewController.swift
+//  Conscious
+//
+//  Created by jeff on 2023/9/19.
+//
+
+import Foundation
+import UIKit
+import EventsCalendar
+
+class PunchRecordViewController: UIViewController {
+
+    lazy var monthCalendarView = {
+        let view = MonthCalendarView(
+            startDate: Date(fromFormattedString: "2023 01 01")!,
+            endDate: Date(fromFormattedString: "2050 12 01")!
+        )
+        view.allowsDateSelection = true // default value: true
+        view.selectedDate = Date()
+
+        view.isPagingEnabled = true // default value: true
+        view.scrollDirection = .horizontal // default value: .horizontal
+        view.viewConfiguration = CalendarConfig() // default valut: .default
+        view.translatesAutoresizingMaskIntoConstraints = false
+        DispatchQueue.main.async {
+               view.scroll(to: Date(fromFormattedString: "2023 09 19")!, animated: false)
+           }
+        return view
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.addSubview(monthCalendarView)
+
+        monthCalendarView.delegate = self
+
+        NSLayoutConstraint.activate([
+            monthCalendarView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100),
+            monthCalendarView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
+            monthCalendarView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
+            monthCalendarView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -300)
+        ])
+    }
+}
+
+extension PunchRecordViewController: CalendarViewDelegate {
+
+    func calendarView(_ calendarView: EventsCalendar.CalendarProtocol, didChangeSelectionDateTo date: Date, at indexPath: IndexPath) {
+        print("selected")
+        // TODO: 顯示當天日記
+    }
+
+
+    func calendarView(_ calendarView: CalendarProtocol,
+                      eventDaysForCalendar type: CalendarViewType,
+                      with calendarInfo: CalendarInfo,
+                      and referenceDate: Date,
+                      completion: @escaping (Result<Set<Int>, Error>) -> ()) {
+
+        FirebaseManager.shared.fetchPunchRecord(userID: "no1") { records, error in
+            if let error = error {
+                print("Error when fetch punch dates: \(error)")
+                completion(.failure(error))
+                return
+            }
+
+            guard let records = records else { return }
+
+            let calendar = Calendar.current
+            let punchDaysInMonth = records.compactMap { records -> Int? in
+                let date = records.punchDate
+                if calendar.isDate(date, equalTo: referenceDate, toGranularity: .month) {
+                    return calendar.component(.day, from: date)
+                }
+                return nil
+            }
+
+            DispatchQueue.main.async {
+                completion(.success(Set(punchDaysInMonth)))
+            }
+
+        }
+    }
+}
