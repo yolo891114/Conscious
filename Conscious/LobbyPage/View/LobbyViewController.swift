b5bab3c0f56b4e229e9022b62b27c5a60f40c6df
jeff <omailong@ssy.local>
2023-10-11 17:35:41 +0800

    fix: Remove crash test button

--- Conscious/LobbyPage/View/LobbyViewController.swift
+++ Conscious/LobbyPage/View/LobbyViewController.swift
@@ -47,16 +47,5 @@ class LobbyViewController: UIViewController {
             self.cloudImage.frame.origin.x = -185
             self.sunImage.transform = CGAffineTransform(rotationAngle: -(.pi / 12))
         })
-
-        let button = UIButton(type: .roundedRect)
-              button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
-              button.setTitle("Test Crash", for: [])
-              button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
-              view.addSubview(button)
     }
-
-    @IBAction func crashButtonTapped(_ sender: AnyObject) {
-          let numbers = [0]
-          let _ = numbers[1]
-      }
 }
